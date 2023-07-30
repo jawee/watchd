21fd04ef9bc49d7a511c23596aa89fc3d564665d
Andreas Olsson <olsson.andreas@gmail.com>
2023-07-30 17:47:37 +0200

    things

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,6 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+use actix_web::{web::{self, Data}, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use sqlx::SqlitePool;
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
@@ -16,13 +8,28 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let connection_pool: SqlitePool = SqlitePool::connect("sqlite://sqlite.db")
+        .await
+        .expect("Couldn't create sqlitepool");
+    let db_data = Data::new(connection_pool.clone());
+
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
-            .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .route("/register", web::post().to(register))
+            .app_data(db_data.clone())
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+async fn register(user: web::Json<RegistrationRequest>) -> impl Responder {
+    return HttpResponse::Ok().json(user);
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RegistrationRequest {
+    pub username: String,
+    pub password: String,
 }
